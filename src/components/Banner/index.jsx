1a24a08091213c46ee6bde3c27e8e79591b856cf
gabrielfari4 <gabrielfaria88@gmail.com>
2025-01-23 16:00:48 -0300

    :sparkles: feat: construindo banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,8 +1,23 @@
 import styles from './Banner.module.css';
+import circuloColorido from 'src/assets/circulo_colorido.png';
+import fotoPassaros from 'src/assets/foto_passaros.jpg'
 
 const Banner = () => {
     return (
-        <div></div>
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Pássaros Top
+                </h1>
+                <p className={styles.paragrafo}>
+                    Bem vindos ao espaço dos pássaros. Aqui estão listados os pássaros mais brabos de todo o reino passaral. Obviamente isso é minha opinião pessoal, que é a única que importa.
+                </p>
+            </div>
+            <div className={styles.imagens}>
+                <img src={circuloColorido} aria-hidden={true} className={styles.circuloColorido}/>
+                <img src={fotoPassaros} alt="Imagem de aves" className={styles.minhaFoto}/>
+            </div>
+        </div>
     )
 }
 
